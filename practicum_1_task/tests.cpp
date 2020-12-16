2a9daac42478b8f8732357f498a8d7ab673391c9
lelkok228 <abalymov.iv@phystech.edu>
2020-12-16 23:05:24 +0300

    delete comments

--- practicum_1_task/tests.cpp
+++ practicum_1_task/tests.cpp
@@ -3,18 +3,6 @@
 #include "gmock/gmock.h"
 #include <iostream>
 
-/*TEST(MaxCommonSuffixTest, test1) {
-	EXPECT_EQ(solve("ab+c.aba.*.bac.+.+*", "babc"), 2);
-}
-
-TEST(MaxCommonSuffixTest, test2) {
-	EXPECT_EQ(solve("acb..bab.c.*.ab.ba.+.+*a.", "cbaa"), 4);
-}
-
-TEST(MaxCommonSuffixTest, test3) {
-	EXPECT_EQ(solve("ab.1+*c.", "abababc"), 7);
-}*/
-
 TEST(MaxCommonSuffixTest, equalityTests) {
 	DerivedSubstring derivedSubstring1 = DerivedSubstring('a', "ba");
 	DerivedSubstring derivedSubstring2 = DerivedSubstring('a', "ca");
